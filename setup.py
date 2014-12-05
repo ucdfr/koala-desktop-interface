38664563b074ad71778903672a8a3dd0b35d6900
Yi Lu <luyi326@gmail.com>
2014-12-05 12:48:55 -0800

    Preliminary setup.py and readme

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='Koala',
+      version='0.1',
+      description='Secret plan of concuring the universe',
+      author='Yi Lu',
+      author_email='mrlu@ucdavis.edu',
+      install_requires=[
+        "virtualenv",
+        "pyserial",
+        "minimalmodbus",
+        "twisted",
+        "autobahn"
+    ]
+     )
---
+++ test.py
@@ -0,0 +1,3 @@
+__author__ = 'yilu'
+
+import PyQt4
